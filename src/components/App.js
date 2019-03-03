c0f9e14a8dd330b6ccce1884d9897869b65cb7b5
Mitch-Tyrer <mltcinematography@gmail.com>
2019-03-02 17:44:55 -0700

    updated state to hold specific galleries for nav links

--- src/components/App.js
+++ src/components/App.js
@@ -17,28 +17,27 @@ import Header from './Header';
 class App extends Component {
 
   state = {
-    imgs: [],
-    search: ''
+    results: [],
+    sunsets: [],
+    cats: [],
+    dogs: []
   }
 
   componentDidMount() {
-    this.performSearch();
+    this.performSearch('sunsets');
+    this.performSearch('cats');
+    this.performSearch('dogs');
   }
 
   performSearch = (query = 'sunsets') => {
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
     .then(res => res.json())
     .then(resData => {
-      this.setState({imgs: resData.photos.photo, search: query});
+      this.setState({[query]: resData.photos.photo});
     })
     .catch(err => {
       console.log('Error fetching and parsing data', err);
     })
-  }
-
-  createGallery = (query) => {
-    this.performSearch(query);
-    return <Gallery data={this.state.imgs} />
   }
 
   render() {
@@ -50,9 +49,9 @@ class App extends Component {
           <SearchForm onSearch={this.performSearch}/>
           <Switch>
               <Route exact path="/" render={ () => <Redirect to="/sunsets" />} />
-              <Route path="/sunsets" render={() => this.createGallery('sunsets')} />
-              <Route path="/cats" render={ () => this.createGallery('cats') } />
-              <Route path="/dogs" render={ () => this.createGallery('dogs') } />
+              <Route path="/sunsets" render={() => <Gallery data={this.state.sunsets} /> } />
+              <Route path="/cats" render={ () => <Gallery data={this.state.cats} /> } />
+              <Route path="/dogs" render={ () => <Gallery data={this.state.dogs} /> } />
           </Switch>
         </div>
       </BrowserRouter>
