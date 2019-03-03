84904ddc99f2521d755156706b88e0a4b1f64a5c
Mitch-Tyrer <mltcinematography@gmail.com>
2019-03-03 00:29:24 -0700

    update 404/NoResults. added loading while fetching data

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,7 @@ import NotFound from './NotFound';
 class App extends Component {
 
   state = {
+    loading: true,
     results: [],
     sunsets: [],
     cats: [],
@@ -31,10 +32,12 @@ class App extends Component {
   }
 
   performSearch = (query, input) => {
+    this.setState({loading: true});
+    
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
     .then(res => res.json())
     .then(resData => {
-      this.setState({[input]: resData.photos.photo});
+      this.setState({[input]: resData.photos.photo, loading: false});
     })
     .catch(err => {
       console.log('Error fetching and parsing data', err);
@@ -51,10 +54,10 @@ class App extends Component {
           <SearchForm onSearch={this.performSearch}/>
           <Switch>
               <Route exact path="/" render={ () => <Redirect to="/sunsets" />} />
-              <Route path="/sunsets" render={() => <Gallery data={this.state.sunsets} /> } />
-              <Route path="/cats" render={ () => <Gallery data={this.state.cats} /> } />
-              <Route path="/dogs" render={ () => <Gallery data={this.state.dogs} /> } />
-              <Route path="/search" render={ () => <Gallery data={this.state.results} /> } />
+              <Route path="/sunsets" render={() => <Gallery loading={this.state.loading} data={this.state.sunsets} /> } />
+              <Route path="/cats" render={ () => <Gallery loading={this.state.loading} data={this.state.cats} /> } />
+              <Route path="/dogs" render={ () => <Gallery loading={this.state.loading} data={this.state.dogs} /> } />
+              <Route path="/search" render={ () => <Gallery loading={this.state.loading} data={this.state.results} /> } />
               <Route component={NotFound} />
           </Switch>
         </div>
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import Image from './Image';
 import NoResults from './NoResults';
+import Loading from './Loading';
+
 
 const Gallery = (props) => {
     const photos = props.data;
-     let imgs;
+    let imgs;
 
-     if(photos.length > 0) {
-         imgs = photos.map( photo => 
+    if (photos.length > 0) {
+        imgs = photos.map(photo =>
             <Image key={photo.id} src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`} />
-            ); 
-     } else {
-         imgs = <NoResults />
-     }
+        );
+    } else {
+        imgs = <NoResults />
+    }
 
     return (
         <div className="photo-container">
             <ul>
-                {imgs}
+                {props.loading ? <Loading /> : imgs}
             </ul>
         </div>
     );
